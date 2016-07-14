d2eed9da5ec1bef5f3b623e910659698fdabe3c2
Gu sir <390512308@qq.com>
2016-07-14 23:17:41 +0800

    add libs

--- build/luajit/build_android_x86.sh
+++ build/luajit/build_android_x86.sh
@@ -1,5 +1,6 @@
 SHELL_PATH=$(cd "$(dirname "$0")"; pwd)
-LUAJIT=../../LuaJIT-2.1.0-beta2
+LUAJIT=$SHELL_PATH/../../LuaJIT-2.1.0-beta2
+OUTPUT=$SHELL_PATH/../../libs/luajit/android/x86
 cd $LUAJIT 
 #set ndk Path. Current use android-ndk-r10d For X86
 NDK=$NDKPATH
@@ -10,7 +11,8 @@ NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-x86"
 make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" clean  
 make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF" 
 
-#cp -f src/libluajit.a ../libluajit/android/x86/libluajit.a
-#cp -f src/luajit ../libluajit/android/x86/luajit
+cp -f $LUAJIT/src/libluajit.a $OUTPUT/libluajit.a
+cp -f $LUAJIT/src/libluajit.so .$OUTPUT/libluajit.so
+cp -f $LUAJIT/src/luajit .$OUTPUT/luajit
 #cp -f src/jit/vmdef.lua ../libluajit/android/x86/vmdef.lua
 

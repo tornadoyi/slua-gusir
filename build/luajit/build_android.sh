6ca0da78e222579fb5d82567478c6cd6c4fcb81c
Gu sir <390512308@qq.com>
2016-07-14 23:40:13 +0800

    add android build

--- build/luajit/build_android.sh
+++ build/luajit/build_android.sh
@@ -1,14 +1,19 @@
-LUAJIT=./luajit
+SHELL_PATH=$(cd "$(dirname "$0")"; pwd)
+LUAJIT=$SHELL_PATH/../../LuaJIT-2.1.0-beta2
+OUTPUT=$SHELL_PATH/../../libs/luajit/android/armeabi-v7a
 cd $LUAJIT 
-#set ndk Path. Current use android-ndk-r10d For ArmV7
-NDK=$ANDROID_NDK
-NDKABI=9  
-NDKVER=$NDK/toolchains/arm-linux-androideabi-4.9  
-NDKP=$NDKVER/prebuilt/linux-x86/bin/arm-linux-androideabi-  
-NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"  
+#set ndk Path. Current use android-ndk-r10d For X86
+NDK=$ANDROID_NDK_HOME
+NDKABI=14
+NDKVER=$NDK/toolchains/arm-linux-androideabi-4.9
+NDKP=$NDKVER/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-
+NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH" clean
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH"
-cp -f src/libluajit.a ../libluajit/android/armeabi-v7a/libluajit.a
-cp -f src/luajit ../libluajit/android/armeabi-v7a/luajit
-cp -f src/jit/vmdef.lua ../libluajit/android/armeabi-v7a/vmdef.lua
+make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH" clean   
+make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
+
+cp -f $LUAJIT/src/libluajit.a $OUTPUT/libluajit.a
+cp -f $LUAJIT/src/libluajit.so $OUTPUT/libluajit.so
+cp -f $LUAJIT/src/luajit $OUTPUT/luajit
+#cp -f src/jit/vmdef.lua ../libluajit/android/x86/vmdef.lua
+
--- build/luajit/build_android_x86.sh
+++ build/luajit/build_android_x86.sh
@@ -3,7 +3,7 @@ LUAJIT=$SHELL_PATH/../../LuaJIT-2.1.0-beta2
 OUTPUT=$SHELL_PATH/../../libs/luajit/android/x86
 cd $LUAJIT 
 #set ndk Path. Current use android-ndk-r10d For X86
-NDK=$NDKPATH
+NDK=$ANDROID_NDK_HOME
 NDKABI=9
 NDKVER=$NDK/toolchains/x86-4.9
 NDKP=$NDKVER/prebuilt/darwin-x86_64/bin/i686-linux-android-
@@ -12,7 +12,7 @@ make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" clean
 make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF" 
 
 cp -f $LUAJIT/src/libluajit.a $OUTPUT/libluajit.a
-cp -f $LUAJIT/src/libluajit.so .$OUTPUT/libluajit.so
-cp -f $LUAJIT/src/luajit .$OUTPUT/luajit
+cp -f $LUAJIT/src/libluajit.so $OUTPUT/libluajit.so
+cp -f $LUAJIT/src/luajit $OUTPUT/luajit
 #cp -f src/jit/vmdef.lua ../libluajit/android/x86/vmdef.lua
 

c160136a7f0c823ddced19273d631ce41104abc4
yi.gu <yi.gu@happyelements.com>
2016-07-15 13:21:33 +0800

    windows slua

--- build/slua/build_windows/build_slua_pbc-master_win_32_64.bat
+++ build/slua/build_windows/build_slua_pbc-master_win_32_64.bat
@@ -1,37 +1,36 @@
-rem compile slua for windows use TDM-GCC-64
+set CUR_PATH=%cd%
+set SEHLL_PATH=%~dp0
+set ROOT_PATH=%SEHLL_PATH%..\..\..
+set LUAJIT_PATH=%ROOT_PATH%\LuaJIT-2.1.0-beta2
+set SLUA_PATH=%ROOT_PATH%\src
+set PBC_PATH=%ROOT_PATH%\3rd\pbc-win
+set LIBS_PATH=%ROOT_PATH%\libs
+set OUTPUT_PATH=%LIBS_PATH%\slua
 
-gcc ..\..\slua.c ^
-	..\pbc-win\src\alloc.c ^
-	..\pbc-win\src\array.c ^
-	..\pbc-win\src\bootstrap.c ^
-	..\pbc-win\src\context.c ^
-	..\pbc-win\src\decode.c ^
-	..\pbc-win\src\map.c ^
-	..\pbc-win\src\pattern.c ^
-	..\pbc-win\src\proto.c ^
-	..\pbc-win\src\register.c ^
-	..\pbc-win\src\rmessage.c ^
-	..\pbc-win\src\stringpool.c ^
-	..\pbc-win\src\varint.c ^
-	..\pbc-win\src\wmessage.c ^
-	..\pbc-win\binding\lua\pbc-lua.c ^
-	-I..\pbc-win ^
-	-o x86_64/slua.dll -m64 -shared -I..\..\luajit/src -Wl,--whole-archive ..\..\libluajit/x86_64/libluajit.a -Wl,--no-whole-archive
+set SRC=%SLUA_PATH%\slua.c ^
+	%PBC_PATH%\src\alloc.c ^
+	%PBC_PATH%\src\array.c ^
+	%PBC_PATH%\src\bootstrap.c ^
+	%PBC_PATH%\src\context.c ^
+	%PBC_PATH%\src\decode.c ^
+	%PBC_PATH%\src\map.c ^
+	%PBC_PATH%\src\pattern.c ^
+	%PBC_PATH%\src\proto.c ^
+	%PBC_PATH%\src\register.c ^
+	%PBC_PATH%\src\rmessage.c ^
+	%PBC_PATH%\src\stringpool.c ^
+	%PBC_PATH%\src\varint.c ^
+	%PBC_PATH%\src\wmessage.c ^
+	%PBC_PATH%\binding\lua\pbc-lua.c ^
 	
-gcc ..\..\slua.c ^
-	..\pbc-win\src\alloc.c ^
-	..\pbc-win\src\array.c ^
-	..\pbc-win\src\bootstrap.c ^
-	..\pbc-win\src\context.c ^
-	..\pbc-win\src\decode.c ^
-	..\pbc-win\src\map.c ^
-	..\pbc-win\src\pattern.c ^
-	..\pbc-win\src\proto.c ^
-	..\pbc-win\src\register.c ^
-	..\pbc-win\src\rmessage.c ^
-	..\pbc-win\src\stringpool.c ^
-	..\pbc-win\src\varint.c ^
-	..\pbc-win\src\wmessage.c ^
-	..\pbc-win\binding\lua\pbc-lua.c ^
-	-I..\pbc-win ^
-	-o x86/slua.dll -m32 -shared -I..\..\luajit/src -Wl,--whole-archive ..\..\libluajit/x86/libluajit.a -Wl,--no-whole-archive
+set INCLUDE=-I%LUAJIT_PATH%\src ^
+	-I%PBC_PATH% ^
+	
+set LIBS_X64=-Wl,--whole-archive %LIBS_PATH%\luajit\x86_64\libluajit.a ^
+	-Wl,--no-whole-archive
+	
+set LIBS_X86=-Wl,--whole-archive %LIBS_PATH%\luajit\x86\libluajit.a ^
+	-Wl,--no-whole-archive	
+	
+gcc %SRC% %INCLUDE% -o %OUTPUT_PATH%\x86_64\slua.dll -m64 -shared %LIBS_X64%
+gcc %SRC% %INCLUDE% -o %OUTPUT_PATH%\x86\slua.dll -m32 -shared %LIBS_X86%
